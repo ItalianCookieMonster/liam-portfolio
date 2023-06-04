76d6b8c466775312a33028c7eb70dbea55da6795
Valentina Toni <vale.toni93@gmail.com>
2023-06-04 20:52:13 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, officia iure aliquam totam sequi reiciendis, iusto soluta laborum earum molestiae labore necessitatibus at error accusamus adipisci fugit illum, maxime obcaecati!</h2>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
